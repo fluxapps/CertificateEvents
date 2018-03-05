869735563a128d219c6ee42049dc8f04a537aa3e
Florian <fw@studer-raimann.ch>
2018-03-05 11:26:29 +0100

    First ILIAS 5.3 support

--- plugin.php
+++ plugin.php
@@ -1,5 +1,5 @@
 <?php
 $id = 'cert_events';
-$version = '1.0.2';
-$ilias_min_version = '5.0.000';
-$ilias_max_version = '5.1.999';
+$version = '1.0.3';
+$ilias_min_version = '5.2.000';
+$ilias_max_version = '5.3.999';
