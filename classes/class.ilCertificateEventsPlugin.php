5b0ff807ec6ba303644085e5b0f3e1bd1a5370ba
Florian <fw@studer-raimann.ch>
2018-03-12 14:59:14 +0100

    Plugin uninstaller

--- classes/class.ilCertificateEventsPlugin.php
+++ classes/class.ilCertificateEventsPlugin.php
@@ -13,47 +13,52 @@ require_once(__DIR__ . '/class.srCertificateEventsCertificateHandler.php');
  */
 class ilCertificateEventsPlugin extends ilEventHookPlugin {
 
-    /**
-     * @param string $component
-     * @param string $event
-     * @param array $parameters
-     */
-    public function handleEvent($component, $event, $parameters) {
-        // Generate certificate if course is completed
-        switch ($component) {
-            case 'Modules/Course':
-                $course = null;
-                if (isset($parameters['object']) && $parameters['object'] instanceof ilObjCourse) {
-                    $course = $parameters['object'];
-                } elseif (isset($parameters['obj_id'])) {
-                    $course = new ilObjCourse(array_pop(ilObject::_getAllReferences($parameters['obj_id'])));
-                }
-                if (!$course) {
-                    return;
-                }
-                $handler = new srCertificateEventsCourseHandler($course);
-                $handler->handle($event, $parameters);
-                break;
-            case 'Certificate/srCertificate':
-                $certificate = $parameters['object'];
-                $handler = new srCertificateEventsCertificateHandler($certificate);
-                $handler->handle($event, $parameters);
-                break;
-        }
-   }
+	/**
+	 * @param string $component
+	 * @param string $event
+	 * @param array  $parameters
+	 */
+	public function handleEvent($component, $event, $parameters) {
+		// Generate certificate if course is completed
+		switch ($component) {
+			case 'Modules/Course':
+				$course = NULL;
+				if (isset($parameters['object']) && $parameters['object'] instanceof ilObjCourse) {
+					$course = $parameters['object'];
+				} elseif (isset($parameters['obj_id'])) {
+					$course = new ilObjCourse(array_pop(ilObject::_getAllReferences($parameters['obj_id'])));
+				}
+				if (!$course) {
+					return;
+				}
+				$handler = new srCertificateEventsCourseHandler($course);
+				$handler->handle($event, $parameters);
+				break;
+			case 'Certificate/srCertificate':
+				$certificate = $parameters['object'];
+				$handler = new srCertificateEventsCertificateHandler($certificate);
+				$handler->handle($event, $parameters);
+				break;
+		}
+	}
 
 
-    /**
-     * Get Plugin Name. Must be same as in class name il<Name>Plugin
-     * and must correspond to plugins subdirectory name.
-     *
-     * Must be overwritten in plugin class of plugin
-     * (and should be made final)
-     *
-     * @return    string    Plugin Name
-     */
-    function getPluginName()
-    {
-        return "CertificateEvents";
-    }
+	/**
+	 * Get Plugin Name. Must be same as in class name il<Name>Plugin
+	 * and must correspond to plugins subdirectory name.
+	 *
+	 * Must be overwritten in plugin class of plugin
+	 * (and should be made final)
+	 *
+	 * @return    string    Plugin Name
+	 */
+	function getPluginName() {
+		return "CertificateEvents";
+	}
+
+
+	protected function beforeUninstall() {
+		// Nothing to delete
+		return true;
+	}
 }
