1fe819f87acb3b1842de90a61ef3f2989cf9d27a
Florian <fw@studer-raimann.ch>
2018-09-13 07:23:57 +0200

    Fix team name

--- plugin.php
+++ plugin.php
@@ -3,3 +3,5 @@ $id = 'cert_events';
 $version = '1.1.0';
 $ilias_min_version = '5.2.000';
 $ilias_max_version = '5.3.999';
+$responsible = 'studer + raimann ag - Team Custom 1';
+$responsible_mail = 'support-custom1@studer-raimann.ch';
